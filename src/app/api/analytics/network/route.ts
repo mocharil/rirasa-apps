25e3e71642229c77e85970a522f86553e7a1b280
Moch. Aril Indra Permana <46191697+mocharil@users.noreply.github.com>
2024-11-07 17:13:15 +0700

    Update route.ts

--- src/app/api/analytics/network/route.ts
+++ src/app/api/analytics/network/route.ts
@@ -22,7 +22,7 @@ export async function GET(request: Request) {
       
       const result = await client.search({
         index: 'twitter_jakarta',
-        size: 100,
+        size: 10000,
         _source: ['username', 'hastags', 'mentions'],
         query: {
           bool: {
